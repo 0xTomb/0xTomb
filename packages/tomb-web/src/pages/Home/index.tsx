36a67b199a1834cae5758de7c4ca55d1cb81797a
heshiliang <heshiliang@bilibili.com>
2022-07-19 12:22:06 +0800

    feat(web): 背景图交接动画

--- packages/tomb-web/src/pages/Home/index.tsx
+++ packages/tomb-web/src/pages/Home/index.tsx
@@ -15,41 +15,46 @@ const HomePage: FC = () => {
     if (!on) {
       setOn(true)
       menuRef.current?.slideMenu()
-      hotZoneRef.current?.classList.add('hidden')
       hotZoneRef.current?.removeEventListener('mouseenter', handleEnterHotZone)
     }
   }
 
   return (
-    <div className={classNames(styles.home, 'h-screen', 'w-screen')}>
+    <div className="h-screen w-screen">
+      <div
+        className={classNames(styles.bg, 'absolute h-screen w-screen transition-all duration-[2000ms]', {
+          invisible: on
+        })}
+      ></div>
+      <div className={classNames(styles.bg2, 'absolute h-screen w-screen', { hidden: !on })}></div>
       <Menu ref={menuRef} />
       <div
-        className="w-10/12 h-4/5 absolute absolute-screen-center z-20"
+        className={classNames('w-10/12 h-4/5 absolute absolute-screen-center z-20', { hidden: on })}
         ref={hotZoneRef}
         onMouseEnter={handleEnterHotZone}
       ></div>
-      <div className="absolute bottom-5 left-8 flex items-end select-none">
-        <img src={GATE1} alt="gate1" className="w-48 h-3/5" />
+      <div className="absolute bottom-5 left-8 flex items-end select-none w-3/4 h-3/4">
+        <img src={GATE1} alt="gate1" className="w-2/12 h-full" />
         <img
           src={DOOR}
-          style={on ? { transform: 'rotateY(-90deg)' } : {}}
+          style={on ? { transform: 'rotateY(-85deg)' } : {}}
           alt="door"
-          className="w-[32rem] h-[40rem] origin-left transition-all duration-[2000ms] transform-gpu"
+          className="w-5/12 h-full origin-left transition-all duration-[2000ms] transform-gpu"
         />
       </div>
       <main
         style={on ? { top: '50%', opacity: 1 } : {}}
-        className="absolute text-5xl font-bold absolute-screen-center transition-all transform-gpu top-16 duration-[2000ms] opacity-0 z-10"
+        className="absolute text-8xl font-bold absolute-screen-center transition-all transform-gpu top-16 duration-[2000ms] opacity-0 z-10 w-fit"
       >
         WELCOME TO 0XTOMB
       </main>
-      <div className="absolute bottom-5 right-8 flex transform -scale-x-[1] items-end select-none">
-        <img src={GATE1} alt="gate1" className="w-48 h-1/2" />
+      <div className="absolute bottom-5 right-8 flex transform -scale-x-[1] items-end select-none w-3/4 h-3/4">
+        <img src={GATE1} alt="gate1" className="w-2/12 h-full" />
         <img
           src={DOOR}
           alt="door"
-          style={on ? { transform: 'rotateY(-90deg)' } : {}}
-          className="w-[32rem] h-[40rem] origin-left transition-all duration-[2000ms] transform-gpu"
+          style={on ? { transform: 'rotateY(-85deg)' } : {}}
+          className="w-5/12 h-full origin-left transition-all duration-[2000ms] transform-gpu"
         />
       </div>
     </div>
